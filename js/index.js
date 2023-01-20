445a3dbc7edeaf93e762062f6e5138b11a8a8a45
Mlarios <mlarios-2021@kinal.edu.gt>
2023-01-20 09:00:49 -0600

    Primer comit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+console.log('hola mundo');
+
+function saludar(){
+
+    let numA = document.getElementById('valorA').value;
+    let numB = document.getElementById('valorB').value;
+
+    let resultado = parseInt(numA) + parseInt(numB);
+    console.log('saludar');
+    console.log(resultado);
+}
